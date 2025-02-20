123b909bc360e4b000c50762a9508e8ff7de332f
Alexey Sazonov <salex.9206@mail.ru>
2025-02-20 21:32:17 +0300

    docs: добавлены JavaDocs для BotBeans

--- bot/src/main/java/backend/academy/bot/BotBeans.java
+++ bot/src/main/java/backend/academy/bot/BotBeans.java
@@ -4,8 +4,15 @@ import com.pengrad.telegrambot.TelegramBot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/** Конфигурационный класс для TelegramBot */
 @Configuration
 public class BotBeans {
+    /**
+     * Создание bean-компонента - объекта класса TelegramBot
+     *
+     * @param botConfig настройки бота (токен и имя)
+     * @return {@code TelegramBot} - объект класса TelegramBot
+     */
     @Bean
     TelegramBot telegramBot(BotConfig botConfig) {
         return new TelegramBot(botConfig.telegramToken());
