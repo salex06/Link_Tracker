04bd67a5fccd3efedaf969d1d8c8ff42ba71359a
Alexey Sazonov <salex.9206@mail.ru>
2025-02-20 21:35:37 +0300

    docs: добавлены JavaDocs для BotConfig

--- bot/src/main/java/backend/academy/bot/BotConfig.java
+++ bot/src/main/java/backend/academy/bot/BotConfig.java
@@ -4,6 +4,12 @@ import jakarta.validation.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
+/**
+ * Класс для представления конфигурации телеграм-бота
+ *
+ * @param telegramToken токен для управления ботом
+ * @param telegramName имя бота
+ */
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record BotConfig(@NotEmpty String telegramToken, @NotEmpty String telegramName) {}
