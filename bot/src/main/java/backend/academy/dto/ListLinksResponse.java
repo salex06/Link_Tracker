1d1765f9ec76db6601ae06344a975aa2928e5eac
Alexey Sazonov <salex.9206@mail.ru>
2025-02-20 22:02:59 +0300

    refactor: ListLinksResponse конвертирован в record
    DTO было конвертировано для улучшения читаемости и уменьшения количества boilerplate кода

--- bot/src/main/java/backend/academy/dto/ListLinksResponse.java
+++ bot/src/main/java/backend/academy/dto/ListLinksResponse.java
@@ -1,52 +1,12 @@
 package backend.academy.dto;
 
 import backend.academy.model.Link;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-/** DTO для передачи набора отслеживаемых ссылок */
-public class ListLinksResponse {
-    /** Набор ссылок на ресурсы */
-    List<Link> links;
-    /** Количество ссылок в DTO */
-    Integer size;
-
-    public ListLinksResponse() {
-        links = new ArrayList<>();
-        size = 0;
-    }
-
-    public ListLinksResponse(List<Link> links, Integer size) {
-        this.links = links;
-        this.size = size;
-    }
-
-    public List<Link> getLinks() {
-        return links;
-    }
-
-    public Integer getSize() {
-        return size;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof ListLinksResponse that)) return false;
-        return Objects.equals(links, that.links) && Objects.equals(size, that.size);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(links, size);
-    }
-
-    public void setLinks(List<Link> links) {
-        this.links = links;
-    }
-
-    public void setSize(Integer size) {
-        this.size = size;
-    }
-}
+/**
+ * DTO для передачи набора отслеживаемых ссылок
+ *
+ * @param links Набор ссылок на ресурсы
+ * @param size Количество ссылок в DTO
+ */
+public record ListLinksResponse(List<Link> links, Integer size) {}
