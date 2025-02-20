a7a94d651f4f2558e314ab8faf83abb7caa8adac
Alexey Sazonov <salex.9206@mail.ru>
2025-02-20 18:59:49 +0300

    docs: добавлены JavaDocs для Command

--- bot/src/main/java/backend/academy/bot/commands/Command.java
+++ bot/src/main/java/backend/academy/bot/commands/Command.java
@@ -1,6 +1,19 @@
 package backend.academy.bot.commands;
 
+/**
+ * Сущность сообщения пользователя - команды бота
+ *
+ * @param commandName название команды
+ * @param needExtraInfo индикатор необходимости дополнительных данных в сообщении с командой
+ */
 public record Command(String commandName, boolean needExtraInfo) {
+    /**
+     * Проверяет сообщение пользователя на соответствие команде
+     *
+     * @param command корректная команда, с которой сравнивается сообщение
+     * @param message сообщение пользователя
+     * @return {@code true} - если сообщение соответствует команде, иначе - {@code false}
+     */
     public static boolean isValidCommand(Command command, String message) {
         if (command.needExtraInfo) {
             String[] splittedMessage = message.split(" ", 2);
