15ea6087319910a9becf37d99c5fd0e874cc7d4c
Alexey Sazonov <salex.9206@mail.ru>
2025-02-21 05:39:58 +0300

    docs: добавлены JavaDocs для Link

--- bot/src/main/java/backend/academy/model/Link.java
+++ bot/src/main/java/backend/academy/model/Link.java
@@ -5,29 +5,53 @@ import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * Представление ссылки на ресурс в системе. Объект класса Link имеет идентификатор, значение ссылки, набор чатов,
+ * отслеживающих ссылку
+ */
 public class Link {
     private Long id;
     private String url;
     private List<Long> tgChatIds;
     private static final AtomicLong nextId = new AtomicLong();
 
+    /**
+     * Конструктор со всеми параметрами
+     *
+     * @param id идентификатор ссылки
+     * @param url значение ссылки
+     * @param tgChatIds чаты, отслеживающие ссылку
+     */
     public Link(Long id, String url, List<Long> tgChatIds) {
         this.id = id;
         this.url = url;
         this.tgChatIds = tgChatIds;
     }
 
+    /**
+     * Конструктор с двумя параметрами
+     *
+     * @param id идентификатор ссылки
+     * @param url значение ссылки
+     */
     public Link(Long id, String url) {
         this.id = id;
         this.url = url;
+        this.tgChatIds = new ArrayList<>();
     }
 
+    /** Конструктор по умолчанию */
     public Link() {
         this.id = null;
         this.url = null;
         this.tgChatIds = new ArrayList<>();
     }
 
+    /**
+     * Конструктор с одним параметром с автоматическим присваиванием идентификатора
+     *
+     * @param url значение ссылки
+     */
     public Link(String url) {
         this.id = nextId.incrementAndGet();
         this.url = url;
