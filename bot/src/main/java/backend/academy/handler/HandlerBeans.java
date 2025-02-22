7978a6808856ebbabcf253ec1ab0da02b531122c
Alexey Sazonov <salex.9206@mail.ru>
2025-02-22 16:51:11 +0300

    docs: добавлены JavaDocs для HandlerBeans

--- bot/src/main/java/backend/academy/handler/HandlerBeans.java
+++ bot/src/main/java/backend/academy/handler/HandlerBeans.java
@@ -5,8 +5,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
+/**
+ * Конфигурационный класс для настройки
+ * классов-обработчиков сообщений
+ */
 @Configuration
 public class HandlerBeans {
+    /**
+     * Возвращает сконструированный объект класса RestClient
+     * @param baseUrl базовый Url
+     * @return объект класса RestClient
+     */
     @Bean
     public RestClient restClient(@Value("${scrapper.base-url}") String baseUrl) {
         return RestClient.builder()
