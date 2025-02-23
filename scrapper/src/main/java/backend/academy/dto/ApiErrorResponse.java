92ba93c7c4a5ffb115ca66e47ddff8ec4bf9db1f
Alexey Sazonov <salex.9206@mail.ru>
2025-02-23 13:09:27 +0300

    fix(ApiErrorResponse): изменено название поля
    Для соответствия DTO с сервисом Bot поле stackTrace переименовано в trace

--- scrapper/src/main/java/backend/academy/dto/ApiErrorResponse.java
+++ scrapper/src/main/java/backend/academy/dto/ApiErrorResponse.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public record ApiErrorResponse(
-        String description, String code, String exceptionName, String exceptionMessage, List<String> stackTrace) {
+        String description, String code, String exceptionName, String exceptionMessage, List<String> trace) {
     public ApiErrorResponse() {
         this("", "", "", "", new ArrayList<>());
     }
