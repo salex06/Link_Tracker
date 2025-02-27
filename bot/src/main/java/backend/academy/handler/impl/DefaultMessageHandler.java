e8b3c9927e868198a5fe3189d5284fac289f5599
Alexey Sazonov <salex.9206@mail.ru>
2025-02-27 20:18:01 +0300

    fix(DefaultMessageHandler): добавлено логирование
    При отправке пользователем неизвестной команды генерируется информационный лог с сообщением

--- bot/src/main/java/backend/academy/handler/impl/DefaultMessageHandler.java
+++ bot/src/main/java/backend/academy/handler/impl/DefaultMessageHandler.java
@@ -4,11 +4,13 @@ import backend.academy.bot.commands.Command;
 import backend.academy.handler.Handler;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
 /** Обработчик неизвестной команды пользователя */
+@Slf4j
 @Order(3)
 @Component
 public class DefaultMessageHandler implements Handler {
@@ -16,6 +18,12 @@ public class DefaultMessageHandler implements Handler {
     public SendMessage handle(Update update, RestClient rest) {
         Long chatId = update.message().chat().id();
         String message = update.message().text();
+
+        log.atInfo()
+                .setMessage("От пользователя получена неизвестная команда")
+                .addKeyValue("chat-id", chatId)
+                .addKeyValue("user-message", message)
+                .log();
 
         return new SendMessage(chatId, "Неизвестная команда: " + message);
     }
