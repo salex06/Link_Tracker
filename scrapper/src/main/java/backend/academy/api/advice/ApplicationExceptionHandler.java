fdbec1b8529a9ba087543c2f6155600ccd802fce
Alexey Sazonov <salex.9206@mail.ru>
2025-02-28 15:49:50 +0300

    feat: дополнен обработчиками RestControllerAdvice
    В класс ApplicationExceptionHandler добавлены обработчики для различных типов исключений. У каждого метода сгенерирована документация в формате JavaDoc

--- scrapper/src/main/java/backend/academy/api/advice/ApplicationExceptionHandler.java
+++ scrapper/src/main/java/backend/academy/api/advice/ApplicationExceptionHandler.java
@@ -6,12 +6,73 @@ import java.util.Arrays;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+/**
+ * Класс предоставляет набор метоодов, которые перехватывают и обрабатывают исключения, сгенерированные в контроллере
+ * ChatController
+ */
 @ControllerAdvice
 public class ApplicationExceptionHandler {
+    /**
+     * Обрабатывает ошибки чтения тела запроса (код 400)
+     *
+     * @param ex объект класса исключения
+     * @return {@code ResponseEntity<ApiErrorResponse>} - форматированный ответ на некорректный запрос
+     */
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiErrorResponse> handleHttpMessageNotReadableException(
+            @NotNull HttpMessageNotReadableException ex) {
+        return handleIncorrectRequest(ex, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * Обрабатывает ошибки преобразования типов (код 400)
+     *
+     * @param ex объект класса исключения
+     * @return {@code ResponseEntity<ApiErrorResponse>} - форматированный ответ на некорректный запрос
+     */
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ApiErrorResponse> handleMethodArgumentTypeMismatchException(
+            @NotNull MethodArgumentTypeMismatchException ex) {
+        return handleIncorrectRequest(ex, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * Обрабатывает исключения вида "не поддерживается тип HTTP метода" (код 405)
+     *
+     * @param ex объект класса исключения
+     * @return {@code ResponseEntity<ApiErrorResponse>} - форматированный ответ на некорректный запрос
+     */
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ApiErrorResponse> handleHttpRequestMethodNotSupportedException(
+            @NotNull HttpRequestMethodNotSupportedException ex) {
+        return handleIncorrectRequest(ex, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    /**
+     * Обрабатывает исключения вида "ресурс (эндпоинт) не найден" (код 404)
+     *
+     * @param ex объект класса исключения
+     * @return {@code ResponseEntity<ApiErrorResponse>} - форматированный ответ на некорректный запрос
+     */
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleNoResourceFoundException(@NotNull NoResourceFoundException ex) {
+        return handleIncorrectRequest(ex, HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Обрабатывает ошибки передачи заголовков (код 400)
+     *
+     * @param ex объект класса исключения
+     * @return {@code ResponseEntity<ApiErrorResponse>} - форматированный ответ на некорректный запрос
+     */
     @ExceptionHandler(MissingRequestHeaderException.class)
     public ResponseEntity<ApiErrorResponse> handleMissingRequestHeaderException(
             @NotNull MissingRequestHeaderException ex) {
