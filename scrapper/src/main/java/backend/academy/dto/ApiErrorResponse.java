190bdb0b293da2c9759a7de41c491f242b05851c
Alexey Sazonov <salex.9206@mail.ru>
2025-03-01 12:18:28 +0300

    docs(Scrapper): добавлены JavaDocs для ApiErrorResponse

--- scrapper/src/main/java/backend/academy/dto/ApiErrorResponse.java
+++ scrapper/src/main/java/backend/academy/dto/ApiErrorResponse.java
@@ -3,6 +3,15 @@ package backend.academy.dto;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Объект передачи данных для ответа на некорректный запрос
+ *
+ * @param description описание ошибки
+ * @param code код ошибки
+ * @param exceptionName название исключения
+ * @param exceptionMessage описание исключения
+ * @param trace трассировка стека
+ */
 public record ApiErrorResponse(
         String description, String code, String exceptionName, String exceptionMessage, List<String> trace) {
     public ApiErrorResponse() {
