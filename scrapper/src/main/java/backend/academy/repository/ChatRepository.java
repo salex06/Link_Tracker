7955357da7062d9917bd68e6e9017d1f13b04ae3
Alexey Sazonov <salex.9206@mail.ru>
2025-03-01 19:18:45 +0300

    docs: добавлены JavaDocs для ChatRepository и MapTgChatRepository

--- scrapper/src/main/java/backend/academy/repository/ChatRepository.java
+++ scrapper/src/main/java/backend/academy/repository/ChatRepository.java
@@ -4,12 +4,36 @@ import backend.academy.model.TgChat;
 import java.util.List;
 import java.util.Optional;
 
+/** Интерфейс предоставляет набор методов для доступа к данным из БД о чатах пользователей с ботом */
 public interface ChatRepository {
+    /**
+     * Получить чат по его идентификатору
+     *
+     * @param id идентификатор чата
+     * @return {@code Optional<TgChat>} если чат найден, иначе - {@code Optional.empty()}
+     */
     Optional<TgChat> getById(Long id);
 
+    /**
+     * Получить список всех чатов из базы данных
+     *
+     * @return список сохраненных чатов
+     */
     List<TgChat> getAll();
 
+    /**
+     * Сохранить чат по его идентификатору
+     *
+     * @param id идентификатор чата
+     * @return {@code true}, если чат успешно сохранен, иначе - false
+     */
     boolean save(Long id);
 
+    /**
+     * Удалить чат по его идентификатору
+     *
+     * @param id идентификатор чата
+     * @return {@code true}, если чат успешно удален, иначе - false
+     */
     boolean remove(Long id);
 }
--- scrapper/src/main/java/backend/academy/repository/impl/MapTgChatRepository.java
+++ scrapper/src/main/java/backend/academy/repository/impl/MapTgChatRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+/** Класс представляет собой хранилище чатов на основе контейнера "Словарь" */
 public class MapTgChatRepository implements ChatRepository {
     private final Map<Long, TgChat> database = new HashMap<>();
 
