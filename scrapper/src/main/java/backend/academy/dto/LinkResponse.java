9048641ab2e22d2c5b042d661f07eee0fcd78c73
Alexey Sazonov <salex.9206@mail.ru>
2025-03-01 11:42:46 +0300

    refactor: синхронизированы LinkResponse в Bot и Scrapper
    В соответствии с контрактом OpenApi добавлены поля tags и filters

--- scrapper/src/main/java/backend/academy/dto/LinkResponse.java
+++ scrapper/src/main/java/backend/academy/dto/LinkResponse.java
@@ -1,8 +1,13 @@
 package backend.academy.dto;
 
-public record LinkResponse(Long id, String url) {
-    public LinkResponse() {
-        this(null, null);
-    }
-    // TODO: добавить поля tags, filters
-}
+import java.util.List;
+
+/**
+ * Объект передачи данных для ответа на запрос на добавление ссылки
+ *
+ * @param id идентификатор ссылки
+ * @param url значение ссылки
+ * @param tags теги
+ * @param filters фильтры
+ */
+public record LinkResponse(Long id, String url, List<String> tags, List<String> filters) {}
