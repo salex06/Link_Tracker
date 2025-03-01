d1bea3a63dabe6c7924b51c2913a890bab63821d
Alexey Sazonov <salex.9206@mail.ru>
2025-03-01 11:40:19 +0300

    refactor: синхронизированы AddLinkRequest в Bot и Scrapper
    Для корректной десериализации в соответствии с контрактом OpenApi переменная, хранящая ссылку на ресурс, переименована с url на link. Также добавлены поля tags и filters

--- bot/src/main/java/backend/academy/dto/AddLinkRequest.java
+++ bot/src/main/java/backend/academy/dto/AddLinkRequest.java
@@ -5,8 +5,8 @@ import java.util.List;
 /**
  * DTO для запроса на отслеживание ресурса по ссылке
  *
- * @param url ссылка на ресурс
+ * @param link ссылка на ресурс
  * @param tags теги ресурса
  * @param filters фильтры для специальных режимов отслеживания ресурса
  */
-public record AddLinkRequest(String url, List<String> tags, List<String> filters) {}
+public record AddLinkRequest(String link, List<String> tags, List<String> filters) {}
--- scrapper/src/main/java/backend/academy/dto/AddLinkRequest.java
+++ scrapper/src/main/java/backend/academy/dto/AddLinkRequest.java
@@ -1,7 +1,12 @@
 package backend.academy.dto;
 
-public record AddLinkRequest(String url) {
-    public AddLinkRequest() {
-        this("");
-    }
-}
+import java.util.List;
+
+/**
+ * Объект передачи данных для запроса на добавление ссылки
+ *
+ * @param link значение ссылки
+ * @param tags теги
+ * @param filters фильтры
+ */
+public record AddLinkRequest(String link, List<String> tags, List<String> filters) {}
