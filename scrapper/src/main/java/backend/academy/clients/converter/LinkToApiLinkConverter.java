155649cfb182c821d6e7bd5745877ec4f1d92cc3
Alexey Sazonov <salex.9206@mail.ru>
2025-03-01 12:47:15 +0300

    docs: добавлены JavaDocs для LinkToApiLinkConverter

--- scrapper/src/main/java/backend/academy/clients/converter/LinkToApiLinkConverter.java
+++ scrapper/src/main/java/backend/academy/clients/converter/LinkToApiLinkConverter.java
@@ -1,5 +1,14 @@
 package backend.academy.clients.converter;
 
+/**
+ * Функциональный интерфейс, предоставляющий возможность конверитровать пользовательскую ссылку в ссылку на api ресурса
+ */
 public interface LinkToApiLinkConverter {
+    /**
+     * Конвертировать пользовательскую ссылку в ссылку на api
+     *
+     * @param link ссылка от пользователя
+     * @return ссылку на api ресурса
+     */
     String convert(String link);
 }
