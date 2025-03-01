349fee2e5f04b708c5be1b8e95b7e6ee74c76c2c
Alexey Sazonov <salex.9206@mail.ru>
2025-03-01 17:31:51 +0300

    docs: добавлены JavaDocs для ChatService

--- scrapper/src/main/java/backend/academy/service/ChatService.java
+++ scrapper/src/main/java/backend/academy/service/ChatService.java
@@ -7,7 +7,10 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+/** Сервис предоставляет методы для обработки запросов на регистрацию или удаление чатов и работу с ними */
+@Service
 public class ChatService {
     private final ChatRepository chatRepository;
 
@@ -16,26 +19,62 @@ public class ChatService {
         this.chatRepository = chatRepository;
     }
 
+    /**
+     * Сохранить чат в базу данных
+     *
+     * @param chatId идентификатор чата
+     * @return {@code true} в случае успешного сохранения, иначе - {@code false}
+     */
     public boolean saveChat(Long chatId) {
         return chatRepository.save(chatId);
     }
 
+    /**
+     * Получить чат по его идентификатору
+     *
+     * @param chatId идентификатор чата
+     * @return {@code Optional<TgChat>} если чат найден, иначе {@code Optional.empty()}
+     */
     public Optional<TgChat> getChat(Long chatId) {
         return chatRepository.getById(chatId);
     }
 
+    /**
+     * Получить список всех чатов
+     *
+     * @return все сохраненные чаты из базы данных
+     */
     public Iterable<TgChat> getAllChat() {
         return chatRepository.getAll();
     }
 
+    /**
+     * Проверить, содержится ли в базе данных чат
+     *
+     * @param chatId идентификатор чата
+     * @return {@code true}, если содержится, иначе - {@code false}
+     */
     public boolean containsChat(Long chatId) {
         return chatRepository.getById(chatId).isPresent();
     }
 
+    /**
+     * Удалить чат из базы данных
+     *
+     * @param chatId идентификатор чата
+     * @return {@code true}, если чат удален, иначе (например, не найден) - {@code false}
+     */
     public boolean deleteChat(Long chatId) {
         return chatRepository.remove(chatId);
     }
 
+    /**
+     * Удалить ссылку из списка отслеживаемых ссылок конкретного чата
+     *
+     * @param chatId идентификатор чата
+     * @param url значение ссылки
+     * @return {@code true}, если ссылка удалена, иначе (например, не найдена) - {@code false}
+     */
     public boolean deleteLink(Long chatId, String url) {
         Optional<TgChat> chat = chatRepository.getById(chatId);
         if (chat.isEmpty()) {
@@ -55,11 +94,23 @@ public class ChatService {
         return true;
     }
 
+    /**
+     * Получить набор отслеживаемых ссылко по идентификатору чата
+     *
+     * @param chatId идентификатор чата
+     * @return множество ссылок (если чат не найден - null)
+     */
     public Set<Link> getChatLinks(Long chatId) {
         Optional<TgChat> tgChat = chatRepository.getById(chatId);
         return tgChat.map(TgChat::links).orElse(null);
     }
 
+    /**
+     * Добавить ссылку как отслеживаемую для конкретного чата
+     *
+     * @param chatId идентификатор чата
+     * @param link значение ссылки
+     */
     public void appendLinkToChat(Long chatId, Link link) {
         Optional<TgChat> chatWrapper = getChat(chatId);
         if (chatWrapper.isPresent()) {
