8227d4539eed6b3d11093fcf270f2c101a853e74
Alexey Sazonov <salex.9206@mail.ru>
2025-03-02 11:05:50 +0300

    fix: добавлен конвертер времени в GitHubRepositoryDTO
    Для корректной конвертации timestamp в LocalDateTime создан внутренний класс-конвертер TimeStampToLocalDateTimeConverter

--- scrapper/src/main/java/backend/academy/clients/github/repository/GitHubRepositoryDTO.java
+++ scrapper/src/main/java/backend/academy/clients/github/repository/GitHubRepositoryDTO.java
@@ -1,8 +1,13 @@
 package backend.academy.clients.github.repository;
 
 import backend.academy.clients.github.GitHubUser;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.util.StdConverter;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 /**
  * Модель репозитория GitHub
@@ -13,9 +18,26 @@ import java.time.LocalDateTime;
  * @param pushedAt время добавления нового коммита в репозиторий
  * @param updatedAt время обновления репозитория
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record GitHubRepositoryDTO(
         @JsonProperty("html_url") String linkValue,
         @JsonProperty("name") String repositoryName,
         @JsonProperty("owner") GitHubUser owner,
         @JsonProperty("pushed_at") LocalDateTime pushedAt,
-        @JsonProperty("updated_at") LocalDateTime updatedAt) {}
+        @JsonProperty("updated_at") @JsonDeserialize(converter = TimeStampToLocalDateTimeConverter.class)
+                LocalDateTime updatedAt) {
+    /** Конвертер времени в формате timestamp (из JSON-объекта) в объект класса LocalDateTime */
+    public static class TimeStampToLocalDateTimeConverter extends StdConverter<String, LocalDateTime> {
+        /**
+         * Конвертировать timestamp в LocalDateTime
+         *
+         * @param time время в строковом виде
+         * @return объекта класса {@code LocalDateTime} - сконвертированное время
+         */
+        @Override
+        public LocalDateTime convert(String time) {
+            Instant instant = Instant.parse(time);
+            return LocalDateTime.ofInstant(instant, ZoneOffset.UTC);
+        }
+    }
+}
