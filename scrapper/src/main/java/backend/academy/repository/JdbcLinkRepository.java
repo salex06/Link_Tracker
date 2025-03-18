60bdcf550d07209e253fac5dcb96ac9b07d1fbff
Alexey Sazonov <salex.9206@mail.ru>
2025-03-18 08:47:41 +0300

    refactor: изменена структура JdbcLinkRepository
    Интерфейс JdbcLinkRepository преобразован в класс, использующий JdbcTemplate для обращения к базе данных. Реализованы методы получения, удаления, изменения, сохранения данных

--- scrapper/src/main/java/backend/academy/repository/JdbcLinkRepository.java
+++ scrapper/src/main/java/backend/academy/repository/JdbcLinkRepository.java
@@ -1,34 +1,147 @@
 package backend.academy.repository;
 
 import backend.academy.model.jdbc.JdbcLink;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface JdbcLinkRepository extends CrudRepository<JdbcLink, Long> {
-    Page<JdbcLink> findAll(Pageable pageable);
+@RequiredArgsConstructor
+public class JdbcLinkRepository {
+    private final JdbcTemplate jdbcTemplate;
 
-    @Query("SELECT * FROM link WHERE link_value = :url")
+    private final NamedParameterJdbcTemplate namedJdbcTemplate;
+
+    private final RowMapper<JdbcLink> jdbcLinkRowMapper = (rs, rn) -> new JdbcLink(
+            rs.getLong("id"),
+            rs.getString("link_value"),
+            rs.getTimestamp("last_update").toLocalDateTime());
+
     @Transactional
-    Optional<JdbcLink> getLinkByUrl(@Param("url") String url);
+    public JdbcLink save(JdbcLink jdbcLink) {
+        if (jdbcLink.getId() == null) {
+            return insertEntity(jdbcLink);
+        }
+        return updateEntity(jdbcLink);
+    }
 
-    @Query("SELECT tg_chat_id FROM tg_chat_link INNER JOIN link ON link_id = id WHERE link_value = :linkValue")
-    Set<Long> getChatIdsByUrl(@Param("linkValue") String url);
+    @Transactional
+    private JdbcLink insertEntity(JdbcLink jdbcLink) {
+        String sql = "INSERT INTO link(link_value) VALUES (:linkValue)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
 
-    @Query(
-            "SELECT * FROM link JOIN tg_chat_link ON tg_chat_link.link_id = link.id WHERE link_value = :linkValue AND tg_chat_id = :chatId")
-    Optional<JdbcLink> getLinkByUrlAndChatId(@Param("chatId") Long chatId, @Param("linkValue") String url);
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("linkValue", jdbcLink.getUrl());
 
-    @Query("SELECT link.id, link.link_value, link.last_update FROM link "
-            + "INNER JOIN tg_chat_link ON link.id = tg_chat_link.link_id "
-            + "WHERE tg_chat_link.tg_chat_id = :chatId")
-    List<JdbcLink> getAllLinksByChatId(@Param("chatId") Long chatId);
+        namedJdbcTemplate.update(sql, params, keyHolder);
+
+        Map<String, Object> keyMap = keyHolder.getKeys();
+        if (keyMap != null && !keyMap.isEmpty()) {
+            Long generatedId = (Long) keyMap.get("id");
+            jdbcLink.setId(generatedId);
+            return jdbcLink;
+        }
+        return null;
+    }
+
+    @Transactional
+    private JdbcLink updateEntity(JdbcLink jdbcLink) {
+        String sql = "UPDATE link SET link_value = :linkValue, last_update = :lastUpdate WHERE id = :id";
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("linkValue", jdbcLink.getUrl());
+        params.addValue("lastUpdate", jdbcLink.getLastUpdateTime());
+        params.addValue("id", jdbcLink.getId());
+
+        namedJdbcTemplate.update(sql, params);
+
+        return jdbcLink;
+    }
+
+    public Page<JdbcLink> findAll(Pageable pageable) {
+        int pageSize = pageable.getPageSize();
+        long offset = pageable.getOffset();
+
+        String sql = "SELECT * FROM link LIMIT :pageSize OFFSET :offset";
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("pageSize", pageSize);
+        params.addValue("offset", offset);
+
+        List<JdbcLink> content = namedJdbcTemplate.query(sql, params, jdbcLinkRowMapper);
+        String countSql = "SELECT COUNT(*) FROM link";
+
+        Long total = namedJdbcTemplate.queryForObject(countSql, new MapSqlParameterSource(), Long.class);
+        if (total == null) {
+            throw new RuntimeException("Unexpected Sql Error");
+        }
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    public Optional<JdbcLink> getLinkByUrl(String url) {
+        String sql = "SELECT * FROM link WHERE link_value = :url";
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("url", url);
+
+        try {
+            JdbcLink link = namedJdbcTemplate.queryForObject(sql, params, jdbcLinkRowMapper);
+            return Optional.of(link);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    public Set<Long> getChatIdsByUrl(String url) {
+        String sql =
+                "SELECT tg_chat_id FROM tg_chat_link INNER JOIN link ON link_id = id WHERE link_value = :linkValue";
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("linkValue", url);
+
+        return new HashSet<>(namedJdbcTemplate.queryForList(sql, params, Long.class));
+    }
+
+    public Optional<JdbcLink> getLinkByUrlAndChatId(Long chatId, String url) {
+        String sql =
+                "SELECT * FROM link JOIN tg_chat_link ON tg_chat_link.link_id = link.id WHERE link_value = :linkValue AND tg_chat_id = :chatId";
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("linkValue", url);
+        params.addValue("chatId", chatId);
+
+        try {
+            JdbcLink link = namedJdbcTemplate.queryForObject(sql, params, jdbcLinkRowMapper);
+            return Optional.of(link);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    public List<JdbcLink> getAllLinksByChatId(@Param("chatId") Long chatId) {
+        String sql = "SELECT link.id, link.link_value, link.last_update FROM link "
+                + "INNER JOIN tg_chat_link ON link.id = tg_chat_link.link_id "
+                + "WHERE tg_chat_link.tg_chat_id = :chatId";
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("chatId", chatId);
+
+        return namedJdbcTemplate.query(sql, params, jdbcLinkRowMapper);
+    }
 }
