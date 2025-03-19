dcacd013501a605002acac36686fd294dc79c2b4
Alexey Sazonov <salex.9206@mail.ru>
2025-03-19 05:44:19 +0300

    test: добавлены тесты для JdbcLinkRepository

---
+++ scrapper/src/test/java/backend/academy/repository/JdbcLinkRepositoryTest.java
@@ -0,0 +1,229 @@
+package backend.academy.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import backend.academy.model.jdbc.JdbcLink;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import javax.sql.DataSource;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = {LiquibaseAutoConfiguration.class})
+class JdbcLinkRepositoryTest {
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private JdbcLinkRepository linkRepository;
+
+    @Container
+    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("link_tracker")
+            .withUsername("postgres")
+            .withPassword("123");
+
+    @BeforeAll
+    static void beforeAll() throws SQLException {
+        postgres.start();
+        System.setProperty("spring.datasource.url", postgres.getJdbcUrl());
+        System.setProperty("spring.datasource.username", postgres.getUsername());
+        System.setProperty("spring.datasource.password", postgres.getPassword());
+    }
+
+    @BeforeAll
+    static void migrateDatabase() throws Exception {
+        try (Connection connection = postgres.createConnection("")) {
+            Database database =
+                    DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+
+            Liquibase liquibase = new Liquibase("migrations/master.xml", new ClassLoaderResourceAccessor(), database);
+            liquibase.update();
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка при выполнении миграций Liquibase", e);
+        }
+    }
+
+    @BeforeEach
+    public void resetTable() {
+        jdbcTemplate.execute("TRUNCATE TABLE link RESTART IDENTITY CASCADE;");
+        jdbcTemplate.execute("TRUNCATE TABLE tg_chat RESTART IDENTITY CASCADE;");
+        jdbcTemplate.execute("ALTER SEQUENCE link_id_seq RESTART WITH 1");
+        jdbcTemplate.execute("ALTER SEQUENCE tg_chat_id_seq RESTART WITH 1");
+    }
+
+    @Test
+    public void getLinkByUrl_WhenLinkExists_ThenReturnCorrectLink() {
+        jdbcTemplate.update("INSERT INTO link(link_value) VALUES ('test_link')");
+
+        Optional<JdbcLink> link = linkRepository.getLinkByUrl("test_link");
+
+        assertThat(link).isNotEmpty();
+        assertThat(link.get().getId()).isEqualTo(1L);
+        assertThat(link.get().getUrl()).isEqualTo("test_link");
+    }
+
+    @Test
+    public void getLinkByUrl_WhenNoLink_ThenReturnEmpty() {
+        jdbcTemplate.update("INSERT INTO link(link_value) VALUES ('another_test_link')");
+
+        Optional<JdbcLink> link = linkRepository.getLinkByUrl("test_link");
+
+        assertThat(link).isEmpty();
+    }
+
+    @Test
+    public void findAll_WhenGetFirstPage_ThenReturnCorrectLinks() {
+        List<String> expectedLinkValues = List.of("test_link1", "test_link2");
+        jdbcTemplate.update("INSERT INTO link(link_value) VALUES ('test_link1')");
+        jdbcTemplate.update("INSERT INTO link(link_value) VALUES ('test_link2')");
+        jdbcTemplate.update("INSERT INTO link(link_value) VALUES ('test_link3')");
+        Pageable pageable = PageRequest.of(0, 2);
+
+        Page<JdbcLink> page = linkRepository.findAll(pageable);
+
+        assertThat(page.getTotalPages()).isEqualTo(2);
+        assertThat(page.getTotalElements()).isEqualTo(3);
+        List<String> links = page.getContent().stream().map(JdbcLink::getUrl).toList();
+        assertThat(links).isEqualTo(expectedLinkValues);
+    }
+
+    @Test
+    public void findAll_WhenGetSecondPage_ThenReturnCorrectLinks() {
+        List<String> expectedLinkValues = List.of("test_link3");
+        jdbcTemplate.update("INSERT INTO link(link_value) VALUES ('test_link1')");
+        jdbcTemplate.update("INSERT INTO link(link_value) VALUES ('test_link2')");
+        jdbcTemplate.update("INSERT INTO link(link_value) VALUES ('test_link3')");
+        Pageable pageable = PageRequest.of(1, 2);
+
+        Page<JdbcLink> page = linkRepository.findAll(pageable);
+
+        assertThat(page.getTotalPages()).isEqualTo(2);
+        assertThat(page.getTotalElements()).isEqualTo(3);
+        List<String> links = page.getContent().stream().map(JdbcLink::getUrl).toList();
+        assertThat(links).isEqualTo(expectedLinkValues);
+    }
+
+    @Test
+    public void getChatIdsByUrl_WhenUrlExists_ThenReturnSetOfIds() {
+        String url = "test_link1";
+        Set<Long> expectedIds = Set.of(1L, 4L);
+        jdbcTemplate.update("INSERT INTO link(link_value) VALUES ('test_link1')");
+        jdbcTemplate.update("INSERT INTO tg_chat(chat_id) VALUES (1), (4)");
+        jdbcTemplate.update("INSERT INTO tg_chat_link(tg_chat_id, link_id) VALUES (1, 1), (4, 1)");
+
+        Set<Long> actualIds = linkRepository.getChatIdsByUrl(url);
+        assertThat(actualIds).isEqualTo(expectedIds);
+    }
+
+    @Test
+    public void getChatIdsByUrl_WhenNoUrl_ThenReturnEmptySet() {
+        String url = "test_link1";
+        jdbcTemplate.update("INSERT INTO link(link_value) VALUES ('another_test')");
+        jdbcTemplate.update("INSERT INTO tg_chat(chat_id) VALUES (1), (4)");
+        jdbcTemplate.update("INSERT INTO tg_chat_link(tg_chat_id, link_id) VALUES (1, 1), (4, 1)");
+
+        Set<Long> actualIds = linkRepository.getChatIdsByUrl(url);
+
+        assertThat(actualIds).isEmpty();
+    }
+
+    @Test
+    public void getLinkByUrlAndChatId_WhenLinkIsConnectedToChat_ThenReturnLink() {
+        String url = "test_link1";
+        Long chatId = 1L;
+        jdbcTemplate.update("INSERT INTO link(link_value) VALUES ('test_link1')");
+        jdbcTemplate.update("INSERT INTO tg_chat(chat_id) VALUES (1)");
+        jdbcTemplate.update("INSERT INTO tg_chat_link(tg_chat_id, link_id) VALUES (1, 1)");
+
+        Optional<JdbcLink> actualLink = linkRepository.getLinkByUrlAndChatId(chatId, url);
+
+        assertThat(actualLink).isNotEmpty();
+        assertThat(actualLink.get().getUrl()).isEqualTo(url);
+    }
+
+    @Test
+    public void getLinkByUrlAndChatId_WhenLinkIsConnectedToAnotherChat_ThenReturnEmpty() {
+        String url = "test_link1";
+        Long chatId = 1L;
+        jdbcTemplate.update("INSERT INTO link(link_value) VALUES ('test_link1')");
+        jdbcTemplate.update("INSERT INTO tg_chat(chat_id) VALUES (1), (2)");
+        jdbcTemplate.update("INSERT INTO tg_chat_link(tg_chat_id, link_id) VALUES (2, 1)");
+
+        Optional<JdbcLink> actualLink = linkRepository.getLinkByUrlAndChatId(chatId, url);
+
+        assertThat(actualLink).isEmpty();
+    }
+
+    @Test
+    public void getLinkByUrlAndChatId_WhenNoLink_ThenReturnEmpty() {
+        String url = "test_link1";
+        Long chatId = 1L;
+
+        Optional<JdbcLink> actualLink = linkRepository.getLinkByUrlAndChatId(chatId, url);
+
+        assertThat(actualLink).isEmpty();
+    }
+
+    @Test
+    public void getAllLinksByChatId_WhenChatHasSomeLinks_ThenReturnListOfLinks() {
+        List<String> expectedUrl = List.of("test_link1", "test_link2");
+        Long chatId = 1L;
+        jdbcTemplate.update("INSERT INTO link(link_value) VALUES ('test_link1'), ('test_link2')");
+        jdbcTemplate.update("INSERT INTO tg_chat(chat_id) VALUES (1)");
+        jdbcTemplate.update("INSERT INTO tg_chat_link(tg_chat_id, link_id) VALUES (1, 1), (1, 2)");
+
+        List<JdbcLink> actualLink = linkRepository.getAllLinksByChatId(chatId);
+
+        assertThat(actualLink.size()).isEqualTo(2);
+        assertThat(actualLink.stream().map(JdbcLink::getUrl).toList()).isEqualTo(expectedUrl);
+    }
+
+    @Test
+    public void getAllLinksByChatId_WhenChatHasNoLinks_ThenReturnEmptyList() {
+        Long chatId = 1L;
+        jdbcTemplate.update("INSERT INTO link(link_value) VALUES ('test_link1'), ('test_link2')");
+        jdbcTemplate.update("INSERT INTO tg_chat(chat_id) VALUES (1)");
+
+        List<JdbcLink> actualLink = linkRepository.getAllLinksByChatId(chatId);
+
+        assertThat(actualLink).isEmpty();
+    }
+
+    @Test
+    public void getAllLinksByChatId_WhenNoChat_ThenReturnEmptyList() {
+        Long chatId = 1L;
+        jdbcTemplate.update("INSERT INTO link(link_value) VALUES ('test_link1'), ('test_link2')");
+
+        List<JdbcLink> actualLink = linkRepository.getAllLinksByChatId(chatId);
+
+        assertThat(actualLink).isEmpty();
+    }
+}
