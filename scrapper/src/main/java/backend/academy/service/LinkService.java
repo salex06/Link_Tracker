36cb66b8d7abf3f5c5b5436ac0311dc0bc23ef14
Alexey Sazonov <salex.9206@mail.ru>
2025-03-21 06:25:19 +0300

    docs: добавлены JavaDoc для LinkService

--- scrapper/src/main/java/backend/academy/service/LinkService.java
+++ scrapper/src/main/java/backend/academy/service/LinkService.java
@@ -9,17 +9,48 @@ import java.util.Set;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+/** Интерфейс сервиса для реализации бизнес-логики работы с ссылками на ресурсы */
 public interface LinkService {
+    /**
+     * Получить ссылки с использованием механизма пагинации
+     *
+     * @param pageable параметры страницы
+     * @return страница с ссылками
+     */
     Page<Link> getAllLinks(Pageable pageable);
 
+    /**
+     * Получить ссылку по идентификатору чата и значению ссылки
+     *
+     * @param chatId идентификатор чата
+     * @param linkValue значение ссылки
+     * @return {@code Optional<Link>}, если ссылка найдена, иначе - {@code Optional.empty()}
+     */
     Optional<Link> getLink(Long chatId, String linkValue);
 
+    /**
+     * Сохранить ссылку для данного чата
+     *
+     * @param link ссылка на ресурс
+     * @param chat телеграм-чат
+     * @return объект класса {@code Link} - сохраненная ссылка
+     */
     Link saveLink(Link link, TgChat chat);
 
-    Link updateChats(Link link, Set<TgChat> newChats);
-
+    /**
+     * Получить все ссылки по идентификатору чата
+     *
+     * @param chatId идентификатор чата
+     * @return набор ссылок, которые отслеживает чат
+     */
     Set<Link> getAllLinksByChatId(Long chatId);
 
+    /**
+     * Получить идентификаторы чатов, которые отслеживают данную ссылку
+     *
+     * @param url значение ссылки
+     * @return набор идентификатор чатов
+     */
     Set<Long> getChatIdsListeningToLink(String url);
 
     /**
