6e18b3c0f80c397f162f3d0b83ef487d923733b7
Alexey Sazonov <salex.9206@mail.ru>
2025-04-07 15:31:12 +0300

    build(Bot): доработаны application.yaml и pom.xml
    Добавлены настройки и зависимости для работы с Kafka

--- bot/src/main/java/backend/academy/bot/BotConfig.java
+++ bot/src/main/java/backend/academy/bot/BotConfig.java
@@ -5,5 +5,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
-@ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
+@ConfigurationProperties(prefix = "app")
 public record BotConfig(@NotEmpty String telegramToken, @NotEmpty String telegramName) {}
